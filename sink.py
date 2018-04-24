e35443532523b11e82b847b5c0757bc9588da7ff
Nicholas Hart <github@hdb3.net>
2018-04-24 14:19:56 +0100

    Update sink.py

--- sink.py
+++ sink.py
@@ -1,14 +1,18 @@
 # sink.py
 
 class Sink:
+from sys import argv
+from os.path import basename,splitext
 
 def __init__(source):
     self.input_type = type(MyInputMessage)
     assert isinstance(source,Source)
     assert source.output_type == self.input_type 
     self.next = source.next
+    self.name = splitext(basename(argv[0]))[0]
+    self.log = lambda s : super.log((self.name,s))
 
-def run():
+def run():self.name
     trace("run starts")
     for msg in self.next:
         trace("process message %d" % n)
