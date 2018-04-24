6beb46836a5c9698b27d7c2cfb86e7b52513023a
Nicholas Hart <github@hdb3.net>
2018-04-24 15:25:18 +0100

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,3 +1,28 @@
-from logger import trace, info, warn, error
+from datetime.datetime import now
 
-def write(s):
+
+TRACE = 4
+INFO  = 3
+WARN  = 2
+ERROR = 1
+
+def __write(s,l,f):
+    global logfile
+    ts = now().strftime("%x %X")
+    logfile.write("%s [%s] %s -- %s" % (ts, self.name.ljust(8),l, s)
+          
+def trace(s):
+    if log_level >= TRACE or self.log_level >= TRACE:
+            __write(s,'TRACE',self)
+                            
+def trace(s):
+    if log_level >= TRACE or self.log_level >= TRACE:
+            __write(s,'TRACE',self)
+                            
+def trace(s):
+    if log_level >= TRACE or self.log_level >= TRACE:
+            __write(s,'TRACE',self)
+                            
+def trace(s):
+    if log_level >= TRACE or self.log_level >= TRACE:
+            __write(s,'TRACE',self)
