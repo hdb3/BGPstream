a1682bba0a22c491e8849aeb10536ca2805db5b6
nic hart <nic@hdb3.net>
2018-04-30 00:24:27 +0100

    add environmant variable control of loglevel

--- framework.py
+++ framework.py
@@ -2,9 +2,10 @@
 
 from basemessage import *
 
-from sys import argv,version_info,exit
+from sys import argv,version_info,exit,stderr
 from os.path import basename,splitext
-from logger import trace, info, warn, error
+import logger
+import os
 
 class Framework:
 
@@ -12,6 +13,14 @@ class Framework:
         if version_info < (3,6):
             error("python version >= 3.6 is required")
             exit()
+
+        env_loglevel = os.environ.get('loglevel')
+        if env_loglevel:
+            logger.set_loglevel(env_loglevel)
+            stderr.write("setting loglevel from environment: [%s]\n" % env_loglevel)
+        else:
+            logger.set_loglevel(logger.SHOW)
+
     
     def run(self):
         error("framework is not expected to be run")
