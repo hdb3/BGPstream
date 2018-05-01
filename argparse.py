0e3cd248ee32e763ade8f75b4ba596df53df3dc9
Nicholas Hart <nic@hdb3.net>
2018-05-01 12:13:39 +0100

    argparse.py

---
+++ argparse.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+
+def __arg_parse(s):
+    parts = s.split('=')
+    if len(parts) == 2:
+        return (parts[0], parts[1])
+    else:
+        sys.exit("could not parse '%s'" % s)
+
+def args_parse():
+    arg_dict = {}
+    arg_list = sys.argv[1:]
+    for arg in arg_list:
+        (k,v) = __arg_parse(arg)
+        arg_dict[k] = v
+    return arg_dict
+
+if __name__ == "__main__":
+    arg_dict = args_parse()
+    print(arg_dict)
