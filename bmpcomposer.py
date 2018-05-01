0a9251f9541c88fe48ccee06c7ab9a7b0d6ff2af
Nicholas Hart <nic@hdb3.net>
2018-05-01 13:46:11 +0100

    cleaed up bmpcomposer.py - now a model for other composers

--- bmpcomposer.py
+++ bmpcomposer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 import sys
 import yaml
 from logger import *
@@ -8,41 +7,10 @@ import bmpparser
 import tcpsource
 import filesource
 import wfBMP
-from argparse import args_parse
+#from argparse import args_parse
 from utils import get_config
 
-
-#from os.path import basename,splitext
-
-def ____get_config():
-
-    extra_config = {}
-    config = {}
-
-    if len(sys.argv) == 1:
-        execpath = os.path.abspath(sys.argv[0])
-        configpath = os.path.splitext(execpath)[0] + '.yml'
-        if os.path.isfile(configpath):
-            warn("using configuration file '%s'" % configpath)
-    elif os.path.isfile(sys.argv[1]):
-        configpath = sys.argv[1]
-        extra_config = args_parse(2)
-    else:
-        info("using command line configuration only")
-        configpath = None
-        extra_config = args_parse(1)
-
-    if configpath:
-        ymlfile = open(configpath, 'r')
-        config = yaml.load(ymlfile)
-
-    config.update(extra_config)
-
-    return config
-
-
 config = get_config()
-print(config)
 
 limit = None
 if 'limit' in config:
